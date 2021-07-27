373332e1def6c20432c33887190800a3f7154453
justin-jiajia <898247676@qq.com>
2021-07-27 10:23:21 +0800

    Update GUI天气.py

--- GUI天气.py
+++ GUI天气.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter.messagebox import *
 import requests
+import json
 
 key = "81f52a7266344015bd30eef507e3c8fd"
 
@@ -15,7 +16,7 @@ except:
     update_time.grid(column=0, row=1)
     showerror("错误", "网络连接出错，加载天气失败！")
 else:
-    w = r.json()
+    w = json.loads(r.content)
     update_time = Label(weather, text="更新日期："+w["now"]["obsTime"][:-6])
     update_time.grid(column=0, row=0)
 
@@ -26,9 +27,24 @@ else:
 
     x = Frame(weather)
     x.grid(column=0, row=2)
-    feels = Label(x, text="体感温度："+w["now"]["feelsLike"])
+    feels = Label(x, text="体感温度："+w["now"]["feelsLike"]+"℃")
     feels.grid(column=0, row=0)
     windDir = Label(x, text="风向："+w["now"]["windDir"])
     windDir.grid(column=1, row=0)
+
+    humidity = Label(x, text="相对湿度："+w["now"]["humidity"]+"%")
+    humidity.grid(column=0, row=1)
+    windScale = Label(x, text="风力等级："+w["now"]["windScale"])
+    windScale.grid(column=1, row=1)
+
+    dew = Label(x, text="露点温度："+w["now"]["dew"]+"℃")
+    dew.grid(column=0, row=2)
+    windSpeed = Label(x, text="风速："+w["now"]["windSpeed"]+"公里/小时")
+    windSpeed.grid(column=1, row=2)
+
+    pressure = Label(x, text="大气压强："+w["now"]["pressure"]+"百帕")
+    pressure.grid(column=0, row=3)
+    vis = Label(x, text="能见度："+w["now"]["vis"]+"公里")
+    vis.grid(column=1, row=3)
 
 weather.mainloop()
